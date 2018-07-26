b5c077e63a95f9f84f9ff6c225b44044e0fbe89a
moshesc <beepbeep9860@gmail.com>
2018-07-26 08:55:07 +0100

    Add project files.

---
+++ ReadFirstLineFromFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadFirstLineFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
